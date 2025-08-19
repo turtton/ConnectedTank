05fca0f036397c1060701bedd9e40d7865924f84
turtton <top.gear7509@turtton.net>
2025-08-20 08:43:55 +0900

    chore: Init commit

---
+++ src/client/kotlin/net/turtton/connectedtank/ConnectedTankClient.kt
@@ -0,0 +1,9 @@
+package net.turtton.connectedtank
+
+import net.fabricmc.api.ClientModInitializer
+
+object ConnectedTankClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/net/turtton/connectedtank/ConnectedTankDataGenerator.kt
@@ -0,0 +1,9 @@
+package net.turtton.connectedtank
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object ConnectedTankDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
