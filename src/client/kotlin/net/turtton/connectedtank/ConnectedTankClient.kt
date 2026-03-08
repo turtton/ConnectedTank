ecc39942cc4263f1a18d792f9effab53e1cc8c84
turtton <top.gear7509@turtton.net>
2026-03-08 18:10:26 +0900

    feat: Add temporary glass texture and cutout render layer for tank block
    datagen でガラステクスチャの cube_all モデルを生成し、
    BlockRenderLayer.CUTOUT を設定してタンク内部の流体描画を可視化。

--- src/client/kotlin/net/turtton/connectedtank/ConnectedTankClient.kt
+++ src/client/kotlin/net/turtton/connectedtank/ConnectedTankClient.kt
@@ -1,12 +1,16 @@
 package net.turtton.connectedtank
 
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap
+import net.minecraft.client.render.BlockRenderLayer
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories
 import net.turtton.connectedtank.block.CTBlockEntityTypes
+import net.turtton.connectedtank.block.CTBlocks
 import net.turtton.connectedtank.block.ConnectedTankBlockEntityRenderer
 
 object ConnectedTankClient : ClientModInitializer {
     override fun onInitializeClient() {
+        BlockRenderLayerMap.putBlock(CTBlocks.CONNECTED_TANK, BlockRenderLayer.CUTOUT)
         BlockEntityRendererFactories.register(CTBlockEntityTypes.CONNECTED_TANK, ::ConnectedTankBlockEntityRenderer)
     }
 }
