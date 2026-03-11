36e83bc5289095a97daf84179997343741f01725
turtton <top.gear7509@turtton.net>
2026-03-11 22:22:05 +0900

    feat: Add crafting recipe for connected tank
    板材とガラスの交互配置パターンでクラフト可能に。
    板材はタグ指定のため全種類の木材に対応。
    
    Closes #4

--- src/client/kotlin/net/turtton/connectedtank/ConnectedTankDataGenerator.kt
+++ src/client/kotlin/net/turtton/connectedtank/ConnectedTankDataGenerator.kt
@@ -1,14 +1,22 @@
 package net.turtton.connectedtank
 
+import java.util.concurrent.CompletableFuture
 import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider
 import net.minecraft.client.data.BlockStateModelGenerator
 import net.minecraft.client.data.ItemModelGenerator
 import net.minecraft.client.data.Models
 import net.minecraft.client.data.TextureMap
 import net.minecraft.client.data.TexturedModel
+import net.minecraft.data.recipe.RecipeExporter
+import net.minecraft.data.recipe.RecipeGenerator
+import net.minecraft.item.Items
+import net.minecraft.recipe.book.RecipeCategory
+import net.minecraft.registry.RegistryWrapper
+import net.minecraft.registry.tag.ItemTags
 import net.minecraft.util.Identifier
 import net.turtton.connectedtank.block.CTBlocks
 
@@ -16,6 +24,7 @@ object ConnectedTankDataGenerator : DataGeneratorEntrypoint {
     override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
         val pack = fabricDataGenerator.createPack()
         pack.addProvider(::ModelProvider)
+        pack.addProvider(::RecipeProvider)
     }
 
     private class ModelProvider(output: FabricDataOutput) : FabricModelProvider(output) {
@@ -31,5 +40,29 @@ object ConnectedTankDataGenerator : DataGeneratorEntrypoint {
         }
 
         override fun generateItemModels(generator: ItemModelGenerator) {}
+    }
+
+    private class RecipeProvider(
+        output: FabricDataOutput,
+        registriesFuture: CompletableFuture<RegistryWrapper.WrapperLookup>,
+    ) : FabricRecipeProvider(output, registriesFuture) {
+        override fun getName(): String = "ConnectedTank Recipes"
+
+        override fun getRecipeGenerator(
+            registryLookup: RegistryWrapper.WrapperLookup,
+            exporter: RecipeExporter,
+        ): RecipeGenerator = object : RecipeGenerator(registryLookup, exporter) {
+            override fun generate() {
+                createShaped(RecipeCategory.DECORATIONS, CTBlocks.CONNECTED_TANK)
+                    .pattern("PGP")
+                    .pattern("GPG")
+                    .pattern("PGP")
+                    .input('P', ItemTags.PLANKS)
+                    .input('G', Items.GLASS)
+                    .criterion("has_planks", conditionsFromTag(ItemTags.PLANKS))
+                    .criterion(hasItem(Items.GLASS), conditionsFromItem(Items.GLASS))
+                    .offerTo(exporter)
+            }
+        }
     }
 }
