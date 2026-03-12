064c659424a90bc7d7554a57a70b6d36b26b320d
turtton <top.gear7509@turtton.net>
2026-03-12 16:13:45 +0900

    feat: Add custom creative tab for ConnectedTank items

--- src/main/kotlin/net/turtton/connectedtank/item/CTItems.kt
+++ src/main/kotlin/net/turtton/connectedtank/item/CTItems.kt
@@ -1,11 +1,14 @@
 package net.turtton.connectedtank.item
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
 import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import net.minecraft.registry.RegistryKey
 import net.minecraft.registry.RegistryKeys
+import net.minecraft.text.Text
 import net.turtton.connectedtank.block.CTBlocks
 import net.turtton.connectedtank.block.TankTier
 import net.turtton.connectedtank.extension.ModIdentifier
@@ -48,5 +51,19 @@ object CTItems {
         return item
     }
 
-    fun init() {}
+    private val ITEM_GROUP_KEY = RegistryKey.of(RegistryKeys.ITEM_GROUP, ModIdentifier("item_group"))
+
+    fun init() {
+        Registry.register(
+            Registries.ITEM_GROUP,
+            ITEM_GROUP_KEY,
+            FabricItemGroup.builder()
+                .displayName(Text.translatable("itemGroup.connectedtank.item_group"))
+                .icon { ItemStack(CONNECTED_TANK) }
+                .entries { _, entries ->
+                    ALL_TANK_ITEMS.forEach { entries.add(it) }
+                }
+                .build(),
+        )
+    }
 }
